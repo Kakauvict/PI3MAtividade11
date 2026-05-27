ba4982d8b96a1194fd137052bb455911fa8ee961
Kakauvict <kauanyvict1@gmail.com>
2026-05-27 16:13:48 -0300

    Create Questao1.py

---
+++ Questao1.py
@@ -0,0 +1,9 @@
+def multiplicar(a, b):
+    resultado = a * b
+    return resultado
+
+
+x = float(input("Digite o primeiro valor: "))
+y = float(input("Digite o segundo valor: "))
+
+print("Resultado:", multiplicar(x, y))
